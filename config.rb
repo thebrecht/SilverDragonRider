71b36be1634315b9a7e8bcfc994cbd346905b7e5
Brecht Huang <mr.brecht@gmail.com>
2011-11-06 09:01:35 +0800

    setup domian

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/SilverDragonRider/"
+http_images_path = "/SilverDragonRider/images"
+http_fonts_path = "/SilverDragonRider/fonts"
+css_dir = "public/SilverDragonRider/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2011-11-05-hello-octopress.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "hello Octopress"
+date: 2011-11-05 14:20
+comments: true
+categories: 
+---
+這是第一篇用Octopress發表的文章，之後《銀龍背上的少女》將會在這里發表
